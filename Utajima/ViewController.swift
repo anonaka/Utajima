ef739f797789470cefc1b3c6ab33576dc6580801
nonaka <nonaka@RoyalCIty2.local>
2014-11-22 09:19:07 +0900

    Initial Commit

---
+++ Utajima/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Utajima
+//
+//  Created by nonaka on 2014/11/22.
+//  Copyright (c) 2014年 nonaka@mac.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
