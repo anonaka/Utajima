a6a0744a0b1c4564f706d0825367752ab2a89a67
Akira Nonaka <nonaka@mac.com>
2014-11-24 19:26:36 +0900

    added view protocol classes.

--- Utajima/UtajimaListTableViewController.swift
+++ Utajima/UtajimaListTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UtajimaListTableViewController: UITableViewController {
+class UtajimaListTableViewController: UITableViewController,UITableViewDataSource,UITableViewDelegate{
 
     var utajimaModel = UtajimaModel()
     
